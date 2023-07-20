a4335571ef249dcb026da2c91027900021a408f0
anconrad <mail@anconrad.de>
2023-07-20 06:06:48 +0200

    iterate

---
+++ iterate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# iterate on live desktop with git push and ansible-pull
+
+set -euxo pipefail
+
+git add .
+git commit -m "iterate"
+git push 
+
+ansible-pull -U https://github.com/anconrad/linux-workstation -e "user=$USER"
