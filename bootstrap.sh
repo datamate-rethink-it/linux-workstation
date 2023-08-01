104fc503f0e87ff094d8e6cdc09026da36b14ea0
Dominic Meyer <dme@seatable.io>
2023-08-01 15:55:53 +0200

    remove one line

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-usermod -aG sudo $USER &&
 sudo apt install git -y &&
 sudo apt install ansible -y &&
 ansible-pull -U https://github.com/datamate-rethink-it/linux-workstation -e "user=$USER" --clean
